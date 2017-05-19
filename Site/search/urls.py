cf7b14ee9304b95e2147c7933f8243d9c0c6b8b4
Kornberg <kornbergfresnel@outlook.com>
2017-05-19 14:52:51 +0800

    construct url route

--- Site/search/urls.py
+++ Site/search/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
+
+app_name = 'search'
 
 urlpatterns = [
-    url(r'^search/', incude('search.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^index/$', )
 ]
