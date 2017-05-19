750c03f551f9e10466c5ed461ccf8e03124baff4
Kornberg <kornbergfresnel@outlook.com>
2017-05-19 21:09:10 +0800

    create data model for searching results

--- Site/search/models.py
+++ Site/search/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class SearchItem(models.Model):
+    title = models.CharField('Title', max_length=120)
+    summary = models.TextField('Summary')
+    url = models.CharField('URL', max_length=225)
+
+    def __str__(self):
+        return self.title
