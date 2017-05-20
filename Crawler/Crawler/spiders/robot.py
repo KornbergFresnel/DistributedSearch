6297f8efa98d0e5436a59f8b2c10969e7cd333f6
Kornberg <kornbergfresnel@outlook.com>
2017-05-20 16:27:59 +0800

    add task before closed

--- Crawler/Crawler/spiders/robot.py
+++ Crawler/Crawler/spiders/robot.py
@@ -18,6 +18,7 @@ class DistributedSpider(scrapy.Spider):
     url_queue = queue.Queue()
     filter_pattern1 = re.compile(pattern='.+\.((jpg)|(ico)|(rar)|(zip)|(doc)|(ppt)|(xls)|(css)|(exe)|(pdf))x?$')
     filter_pattern2 = re.compile(pattern='^((javascript:)|(openapi)).+')
+    BLOCK_URLS = ""
 
     def start_requests(self):
         # load start urls from outer files
@@ -27,7 +28,22 @@ class DistributedSpider(scrapy.Spider):
         while url_queue.empty() is not True:
             yield scrapy.Request(url=url_queue.get(), callback=self.parse)
 
+    @classmethod
+    def from_crawler(self, crawler, *args, **kwargs):
+        spider = super(DistributedSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+
     def parse(self, response):
+        """This function is mainly do the parsing task,
+        we should focus on the depth of BFO and the relative urls,
+        when the depth of crawling is over thant the maximum, we block current url, then note it with BLOCK_URLS
+        when we meet a relative urls, we check it then bind it with DOMAIN
+        """
+        # first check whether current depth is maximum, store current url
+        if response.meta["depth"] > settings.DEPTH_LIMIT:
+            self.BLOCK_URLS += response.url + '\n'
+            return
         # get current domain url
         domain = pretask.getDomain(response.url)
         item = CrawlerItem()
@@ -52,5 +68,18 @@ class DistributedSpider(scrapy.Spider):
                 url = domain + url
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def spider_closed(self, reason):
+        # store before we close our spider completely
+        self.store()
+        spider.logger.info('%s data has been store!!!' % len(self.BLOCK_URLS))
+
     def illegal(self, ori_url):
+        """If ori_url is not a visitable url, this function will return true
+        """
         return re.match(str=ori_url, pattern=self.filter_pattern1) and re.match(str=ori_url, pattern=self.filter_pattern2)
+
+    def store(self):
+        """Store block urls which stoped for depth has over the maximum
+        """
+        with open(settings.STORAGE_FILE, 'w') as f:
+            f.write(self.BLOCK_URLS)
