e19749ee9cb3749e7725f4b5df16ccf5bbbd8733
Kornberg <kornbergfresnel@outlook.com>
2017-05-27 10:22:58 +0800

    implement search view function

--- Site/search/views.py
+++ Site/search/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
 from .forms import SearchForm
 from .models import SearchItem
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 import Crawler.Crawler.Search.search as Search
-import json
+import datetime
 
 
 # Create your views here.
 def index(request):
     # if this is a POST request we need to process the form data
-    if request.method == "GET":
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['q']
-            context = SearchItem.objects.all()
-            return render(request, 'search/result.html', {'context': context, 'form': form})
-    else:
-        form = SearchForm()
-    return render(request, 'search/index.html', {'form': form})
+    now = datetime.datetime.now()
+    return render(request, 'search/index.html', {'current_time': now})
 
 
 def search(request):
     """Handling search request
     """
+    if 'q' in request.GET:
+        q = request.GET['q']
+        # results = SearchItem.objects.all()
+        results = Search.search_request(q)
+        return render(request, 'search/result.html', {'results': results, 'query': q})
